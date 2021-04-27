1558e9d209625651e8ec8ba72af0d45dbf24b989
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-04-27 15:39:25 +0200

    more on form

--- testingreact/src/App.js
+++ testingreact/src/App.js
@@ -27,9 +27,14 @@ function App() {
     },
   ];
 
+  function addExpenseHandler (expense) {
+    console.log('in app js');
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
