f0898d2b9c4332d8c7dbb850d9d8717a0ba0677c
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-05-27 16:51:08 +0200

    filter array, conditional rendering

--- testingreact/src/components/Expenses/Expenses.js
+++ testingreact/src/components/Expenses/Expenses.js
@@ -11,12 +11,24 @@ function Expenses(props) {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+    /* olika sätt att göra conditional rendering, standard sättet bortkommenterat */
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))} 
+    
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map((expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
-
+                {expensesContent}
+                {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+                {filteredExpenses.length > 0 && filteredExpenses.map((expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))} */}
             </Card>
         </div>
 
